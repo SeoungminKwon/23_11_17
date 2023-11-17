04f32b3895c4d0582c9b0ab43e5f7c18dc75a325
SeoungminKwon <tjdalsrnjs10@gmail.com>
2023-11-17 09:58:05 +0900

    11_17_1

---
+++ src/main/java/com/example/sb_prac2/SbPrac2Application.java
@@ -0,0 +1,13 @@
+package com.example.sb_prac2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SbPrac2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SbPrac2Application.class, args);
+    }
+
+}
